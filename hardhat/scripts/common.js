e83bdfacec1fad7d69cd3644a25f2aa81c35a932
solidoracle <solidoracle17@gmail.com>
2023-11-18 21:04:43 +0300

    erc4626 with ml rebalancing

--- hardhat/scripts/common.js
+++ hardhat/scripts/common.js
@@ -1,4 +1,4 @@
-exports.ABI = [
+exports.ABI_VAULT = [
     // IErc20 Interface
     'function name() external pure returns (string memory)',
     'function symbol() external pure returns (string memory)',
@@ -10,8 +10,32 @@ exports.ABI = [
     'function allowance(address owner, address spender) external view returns (uint256)',
 
     // Vault Interface, extends IErc20
+
     'function deposit(uint256 amount) external payable',
     'function withdraw(uint256 amount) external',
+    'function setAsset(address asset_address) external',
+    'function asset() external view returns (address)',
 ];
 
-exports.ADDRESS = '0x1cD1A83C2965CB7aD55d60551877Eb390e9C3d7A';
+exports.ADDRESS_VAULT = '0x6982aA8dB18E7153c614765dA4A581135e9Bf4a8';
+
+
+exports.ABI_WETH = [
+    // IErc20 Interface
+    'function name() external pure returns (string memory)',
+    'function symbol() external pure returns (string memory)',
+    'function decimals() external pure returns (uint8)',
+    'function balanceOf(address _address) external view returns (uint256)',
+    'function transfer(address to, uint256 value) external returns (bool)',
+    'function approve(address spender, uint256 value) external returns (bool)',
+    'function transferFrom(address from, address to, uint256 value) external returns (bool)',
+    'function allowance(address owner, address spender) external view returns (uint256)',
+
+    // Vault Interface, extends IErc20
+
+    'function deposit(uint256 amount) external payable',
+    'function withdraw(uint256 amount) external',
+    'function setAsset(address asset_address) external'
+];
+
+exports.ADDRESS_WETH = '0xa9a04d0C22B6f264BC72a108d124f25BD199c928';
--- hardhat/scripts/initialize.js
+++ hardhat/scripts/initialize.js
@@ -1,12 +1,19 @@
 const { ethers } = require('hardhat');
-const { ABI, ADDRESS } = require('./common');
+const { ABI_VAULT, ADDRESS_VAULT, ABI_WETH, ADDRESS_WETH } = require('./common');
+const chai = require('chai');
+const { expect } = chai;
 
 async function main() {
     const [signer] = await ethers.getSigners();
 
-    const vault = await ethers.getContractAt(ABI, ADDRESS, signer);
-    
+    const vault = await ethers.getContractAt(ABI_VAULT, ADDRESS_VAULT, signer);
+    const weth = await ethers.getContractAt(ABI_WETH, ADDRESS_WETH, signer);
 
+    await vault.setAsset(weth.target);
+
+    const weth_address = await vault.asset();
+    expect(weth_address).to.equal(ADDRESS_WETH);
+   
     const amountToDeposit = ethers.parseEther("0.000000000000001");
 
     await vault.deposit(amountToDeposit);
